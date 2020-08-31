7bcf32c9e152610c401cd4ae8db84e2d7a904a77
Santosh Gurujula <42249090+santoshgurujula@users.noreply.github.com>
2020-08-31 22:46:35 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ML capstone project for stock price prediction',
+    author='santoshgurujula@gmail.com',
+    license='MIT',
+)
